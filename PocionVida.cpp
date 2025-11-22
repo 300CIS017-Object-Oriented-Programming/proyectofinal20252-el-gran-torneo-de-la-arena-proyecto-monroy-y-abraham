3199cfd9bd99e156d65df46d06f74f1006fcfcf9
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-21 20:53:35 -0500

    implento la fucion de pocion vida que lo que hace es retaura la vida del heroe y buscabos en chat gpt la manera de ordenar los texto con simbolos entre corchetes  [ ] diciendo que va a suceder ejemplo [info]

--- PocionVida.cpp
+++ PocionVida.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "PocionVida.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+PocionVida::PocionVida(int stock)
+    : ObjetoMagico("PocionVida", "Restaura entre 20-40 puntos de vida", stock, "curacion") {
+
+    this->curacionMin = 20;
+    this->curacionMax = 40;
+}
+
+PocionVida::~PocionVida() {
+    // Destructor
+}
+
+void PocionVida::usar(Personaje* objetivo) {
+    if (!objetivo || !objetivo->estaVivo()) {
+        cout << " No se puede usar en este objetivo" << endl;
+        return;
+    }
+
+    // Calcular curación aleatoria
+    int curacion = curacionMin + (rand() % (curacionMax - curacionMin + 1));
+
+    cout << "   [INFO] Efecto: Restaura " << curacion << " puntos de vida" << endl;
+    objetivo->curar(curacion);
+
+    marcarUsado();
+}
