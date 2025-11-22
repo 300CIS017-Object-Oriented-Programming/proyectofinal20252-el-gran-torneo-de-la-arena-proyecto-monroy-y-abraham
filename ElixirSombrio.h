d04df47059a3153f4bc4b59a52d06933c870d009
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-22 16:07:43 -0500

    implemento elixir sombrio al hablar con mi compañero decidimos que este objetos afecte todos los personajes del juego y haga daño a todos es decir lo usa y afecta tanto al heroe y al villano

--- ElixirSombrio.h
+++ ElixirSombrio.h
@@ -5,8 +5,18 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_ELIXIRSOMBRIO_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_ELIXIRSOMBRIO_H
 
+#include "ObjetoMagico.h"
+#include "Personaje.h"
+#include <vector>
 
-class ElixirSombrio {
+class ElixirSombrio : public ObjetoMagico {
+private:
+    int danioMin;
+    int danioMax;
+
+public:
+    ElixirSombrio(int stock);
+    void usar(Personaje* objetivo) override;
 };
 
 
