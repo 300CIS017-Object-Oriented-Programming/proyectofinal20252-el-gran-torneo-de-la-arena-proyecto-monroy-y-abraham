ee69ef48cd976d753b4cb95bfa0a7ea2a21bce44
Guzmán <jjguzmandiaz10@gmail.com>
2025-11-22 11:01:45 -0500

    crescion de sanador cpp donde definimos todos los metodos y llamamos todo del .h

--- Sanador.h
+++ Sanador.h
@@ -4,7 +4,24 @@
 
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_SANADOR_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_SANADOR_H
-#incliude "Perosnaje.h"
+#incliude "Personaje.h"
 
+class Sanador : public Personaje {
+private:
+    int curacionBase;
+    float efectividadMin;
+    float efectividadMax;
 
+    // RECURSO: MANA
+    int mana;
+    int manaMaximo;
+
+public:
+    Sanador(string nombre, int nivel);
+    ~Sanador();
+
+    int atacar(Personaje* objetivo) override;
+    void realizarAccion(vector<Personaje*>& aliados, vector<Personaje*>& enemigos) override;
+
+};
 #endif //PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_SANADOR_H
