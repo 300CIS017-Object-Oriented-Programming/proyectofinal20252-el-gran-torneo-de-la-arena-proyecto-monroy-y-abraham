690bf7db05654eb3da8ff75aee993be2ffe6d2ff
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-22 10:58:23 -0500

    implemento el paladin .h con sus atributos que va a ser ese heroe el cual hereda de personaje

--- Paladin.h
+++ Paladin.h
@@ -5,8 +5,29 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_PALADIN_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_PALADIN_H
 
+#include "Personaje.h"
 
-class Paladin {
+class Paladin : public Personaje {
+private:
+    int defensaExtra;
+    int danioSagrado;
+    int energiaSacra;
+    int energiaMaxima;
+
+public:
+
+    Paladin(string nombre, int nivel);
+    ~Paladin();
+
+    int atacar(Personaje* objetivo) override;
+    void realizarAccion(vector<Personaje*>& aliados, vector<Personaje*>& enemigos) override;
+
+private:
+
+    int ataqueDefensivo(Personaje* objetivo);
+    void bendicionDivina();
+    void plegaria(); // recurpar energia del personaje
+
 };
 
 
