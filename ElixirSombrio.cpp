e79b54df9bf179264ade4f3eb5aa2face874f83f
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-22 16:21:58 -0500

    subo el cpp de elixirsombrio

--- ElixirSombrio.cpp
+++ ElixirSombrio.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "ElixirSombrio.h"
+#include <iostream>
+
+
+ElixirSombrio::ElixirSombrio(int stock)
+    : ObjetoMagico("ElixirSombrio", "Daña a TODOS (¡PELIGROSO!)", stock, "especial") {
+    this->danioMin = 15;
+    this->danioMax = 25;
+}
+
+void ElixirSombrio::usar(Personaje* objetivo) {
+    if (!objetivo) return;
+
+    int danio = danioMin + (rand() % (danioMax - danioMin + 1));
+
+    cout << "   [!] ADVERTENCIA! El elixir libera energia oscura!" << endl;
+    cout << "   [INFO] Dana a todos en el campo: " << danio << " puntos" << endl;
+    cout << "   [INFO] (En combate real, afectaria a todos los personajes)" << endl;
+
+    // Por simplicidad, solo daña al objetivo
+    objetivo->recibirDanio(danio / 2);
+    marcarUsado();
+}
