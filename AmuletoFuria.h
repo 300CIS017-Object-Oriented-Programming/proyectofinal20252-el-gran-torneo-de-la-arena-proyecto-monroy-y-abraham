ee743e60e76e5395965ebb3c5aa085279d696865
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-22 15:59:35 -0500

    con min compañero decidimos que amuletofuria que hereda de objeto magico lo que hace este objeto es aumentar el ataque del personaje que lo usa

--- AmuletoFuria.h
+++ AmuletoFuria.h
@@ -5,9 +5,22 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_AMULETOFURIA_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_AMULETOFURIA_H
 
+#include "ObjetoMagico.h"
+#include "Personaje.h"
 
-class AmuletoFuria {
+class AmuletoFuria : public ObjetoMagico {
+private:
+    int aumentoMin;
+    int aumentoMax;
+    int duracion;
+
+public:
+    AmuletoFuria(int stock);
+    ~AmuletoFuria();
+
+    void usar(Personaje* objetivo) override;
 };
+
 
 
 #endif //PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_AMULETOFURIA_H
