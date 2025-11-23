f90a3460140f06986468279b06d2c0b300fd3a2c
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-23 13:46:30 -0500

    subo el cpp de escudobendito

--- EscudoBendito.cpp
+++ EscudoBendito.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "EscudoBendito.h"
+#include <iostream>
+
+using namespace std;
+
+EscudoBendito::EscudoBendito(int stock)
+    : ObjetoMagico("EscudoBendito", "Aumenta defensa +10 a +20 por 1 turno", stock, "buff_defensa") {
+
+    this->defensaMin = 10;
+    this->defensaMax = 20;
+    this->duracion = 1;
+}
+
+EscudoBendito::~EscudoBendito() {
+}
+
+void EscudoBendito::usar(Personaje* objetivo) {
+    if (!objetivo || !objetivo->estaVivo()) {
+        cout << " No se puede usar en este objetivo" << endl;
+        return;
+    }
+
+    int aumento = defensaMin + (rand() % (defensaMax - defensaMin + 1));
+
+    cout << "   [INFO] Efecto: Aumenta defensa +" << aumento << " por " << duracion << " turno" << endl;
+    objetivo->aplicarEfecto("defensa", aumento, duracion);
+
+    marcarUsado();
+}
