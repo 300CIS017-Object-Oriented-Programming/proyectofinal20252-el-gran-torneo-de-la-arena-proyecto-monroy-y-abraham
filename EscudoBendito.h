241534b3716596bf66dbdec4f1731b00b7117774
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-23 07:56:24 -0500

    subo escudobendito.h lo cual sera un escudo para los personajes que lo usen tambien no hemos subir mas commit por que tuvimos incoveniente con mi compañero pero ya se han solucionado

--- EscudoBendito.h
+++ EscudoBendito.h
@@ -5,8 +5,20 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_ESCUDOBENDITO_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_ESCUDOBENDITO_H
 
+#include "ObjetoMagico.h"
+#include "Personaje.h"
 
-class EscudoBendito {
+class EscudoBendito : public ObjetoMagico {
+private:
+    int defensaMin;
+    int defensaMax;
+    int duracion;
+
+public:
+    EscudoBendito(int stock);
+    ~EscudoBendito();
+
+    void usar(Personaje* objetivo) override;
 };
 
 
