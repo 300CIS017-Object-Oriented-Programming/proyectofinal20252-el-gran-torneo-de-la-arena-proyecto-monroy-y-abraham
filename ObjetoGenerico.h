875d85d2fd580f736ef2d8272252af18a751a63a
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-24 10:48:55 -0500

    subo el punto h del objeto generico el cual perminte crear un objeto nuevo al usuario

--- ObjetoGenerico.h
+++ ObjetoGenerico.h
@@ -5,8 +5,19 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_OBJETOGENERICO_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_OBJETOGENERICO_H
 
+class ObjetoGenerico : public ObjetoMagico {
+private:
+    string tipoEfecto; // "curacion", "buff_ataque", "buff_defensa", "especial"
+    int valorMin;
+    int valorMax;
+    int duracion;
 
-class ObjetoGenerico {
+public:
+    ObjetoGenerico(string nombre, string descripcion, int stock,
+                   string tipoEfecto, int valorMin, int valorMax, int duracion = 0);
+    ~ObjetoGenerico();
+
+    void usar(Personaje* objetivo) override;
 };
 
 
