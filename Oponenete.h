dea69aa375a1633f4ece09b12c8a1bca0398b6ee
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-25 08:59:42 -0500

    he implentado oponente.h que ya seria el oponente del juegos que como habia dialogado comi compañero es el oponente es todo aleatorio

--- Oponenete.h
+++ Oponenete.h
@@ -5,8 +5,27 @@
 #ifndef PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_OPONENETE_H
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_OPONENETE_H
 
+#include "Personaje.h"
 
-class Oponenete {
+class Oponente : public Personaje {
+private:
+    string estrategia; // "agresivo", "defensivo", "equilibrado"
+    int agresividad;
+    int contadorTurnos;
+
+public:
+    Oponente(string nombre, int nivel, int vida, int ataque, int defensa, string rol);
+    ~Oponente();
+
+    int atacar(Personaje* objetivo) override;
+    void realizarAccion(vector<Personaje*>& aliados, vector<Personaje*>& enemigos) override;
+
+private:
+    void decidirAccion(vector<Personaje*>& aliados, vector<Personaje*>& enemigos);
+    Personaje* seleccionarObjetivo(vector<Personaje*>& enemigos);
+    Personaje* seleccionarMasDebil(vector<Personaje*>& enemigos);
+    Personaje* seleccionarMasFuerte(vector<Personaje*>& enemigos);
+    int contarVivos(vector<Personaje*>& personajes);
 };
 
 
