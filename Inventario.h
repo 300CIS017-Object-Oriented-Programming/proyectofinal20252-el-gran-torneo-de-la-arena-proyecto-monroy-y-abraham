86c737fc192a8805d5eb56f1de1ba31f4a46250a
abraham21587 <abrahamwaggon821@gmail.com>
2025-11-25 20:30:10 -0500

    Se incorpora Inventario.h como módulo de gestión de recursos, definiendo estructura y operaciones del inventario global

--- Inventario.h
+++ Inventario.h
@@ -6,8 +6,47 @@
 #define PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_INVENTARIO_H
 
 
-class Inventario {
-};
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "ObjetoMagico.h"
+#include "Personaje.h"
 
+using namespace std;
+
+class Inventario {
+private:
+    unordered_map<string, ObjetoMagico*> objetosDisponibles;
+    vector<ObjetoMagico*> objetosUsados;
+    int capacidadMaxima;
+
+public:
+    Inventario();
+    ~Inventario();
+
+    void inicializarObjetos();
+    bool crearObjeto(ObjetoMagico* objeto);
+    void listarObjetos();
+    void listarObjetosDetallado();
+    ObjetoMagico* consultarObjeto(string nombre);
+    bool actualizarStock(string nombre, int nuevoStock);
+    bool eliminarObjeto(string nombre);
+    bool asignarObjetoAHeroe(Personaje* heroe, string nombreObjeto);
+    bool retirarObjetoDeHeroe(Personaje* heroe, string nombreObjeto);
+    bool verificarDisponibilidad(string nombre);
+    void limpiarObjetosUsados();
+
+
+    // Retorna un vector con todos los objetos disponibles ayuda de un compañero
+    vector<ObjetoMagico*> getObjetosDisponibles() {
+        vector<ObjetoMagico*> objetos;
+        for (auto& par : objetosDisponibles) {
+            objetos.push_back(par.second);
+        }
+        return objetos;
+    }
+
+    vector<ObjetoMagico*> getObjetosUsados() { return objetosUsados; }
+};
 
 #endif //PROYECTOFINAL20252_EL_GRAN_TORNEO_DE_LA_ARENA_PROYECTO_MONROY_Y_ABRAHAM_INVENTARIO_H
